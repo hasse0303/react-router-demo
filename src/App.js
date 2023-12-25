39efb53c400af603a8b87bde87c7ee5de5726b72
Has Se <has.se@ecoinsoft.com>
2023-12-25 11:48:57 +0700

    Use Link component to navigate route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { Routes, Route} from 'react-router-dom'
 import './App.css';
 import { Home } from './components/Home';
 import { About } from './components/About';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='about' element={<About />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+      </Routes>
+    </>
   );
 }
 
