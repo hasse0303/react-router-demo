bfe19396092a78f00d19791873d3f3e2737622ba
Has Se <has.se@ecoinsoft.com>
2023-12-25 12:00:34 +0700

    Active link without props style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,10 @@ import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
 
-    const handleStyle = ({ isActive }) => {
-        return {
-            textDecoration: isActive ? 'none' : 'underline',
-            fontWeight: isActive ? 'bold' : 'normal',
-        }
-    } 
   return (
     <nav>
-        <NavLink style={handleStyle} to={'/'}>Home</NavLink>
-        <NavLink style={handleStyle} to={'/about'}>About</NavLink>
+        <NavLink to={'/'}>Home</NavLink>
+        <NavLink to={'/about'}>About</NavLink>
     </nav>
   )
 }
